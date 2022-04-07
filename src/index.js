9d203b5ecc1a06768ff544eb6c1740131041b8fa
sgreene-code <stgreene@outlook.com>
2022-04-07 15:09:46 -0500

    Got some of the ball object with liquid fun working
    Stuck on line 38.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 import ballImg from './assets/ball.png';
 
+
 var timeStep = 1.0/60.0;
 var velocityIterations = 8;
 var positionIterations = 3;
+var bd = new b2BodyDef;
+
 class MyGame extends Phaser.Scene
 {
     constructor ()
@@ -22,10 +25,17 @@ class MyGame extends Phaser.Scene
     {
         this.myBallSprite = this.add.image(300,100,'ball');
         const logo = this.add.image(400, 150, 'logo');
-        var gravity = new b2Vec2(0,-10.0);
-      
-        
+        var gravity = new b2Vec2(0,-10.0);  
         this.myWorld = new b2World(gravity);
+        this.ground = this.myWorld.CreateBody(bd);
+        this.ball = new b2BodyDef();
+        this.circle = new b2CircleShape();
+        this.ball.type = b2_dynamicBody;
+    
+        this.testCircle = this.myWorld.CreateBody(this.ball);
+        this.circle.position.Set(0,20);
+        this.circle.radius = 1.75;
+        //this.testCircle.CreateFixtureFromShape(this.circle,0.5);
 
 
         this.tweens.add({
