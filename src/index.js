5ea78c4de8a4f667e7679dbcc14f030085dde189
Maximilian Weiher <mweiher@macalester.edu>
2022-04-25 22:32:55 -0500

    added rough hoop
    added basketball hoop with physics
    surprisingly, the layering idea i had worked perfectly and the jello stays in front of the backboard and behind the hoop!
    there's a clear difference in speed between the two flippers despite the having the same numbers. something to investigate

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import player1Img from './assets/sprites/player1.png';
 import player2Img from './assets/sprites/player2.png';
 import jelloImg from './assets/jello.png';
 import coolwhipImg from './assets/sprites/coolwhip.png';
+import backboardImg from './assets/sprites/backboard.png';
+import hoopImg from './assets/sprites/hoop.png';
 
 import LiquidFunPhysics from './lf-phaser.js';
 
@@ -23,17 +25,19 @@ class MyGame extends Phaser.Scene
         this.load.image('player2', player2Img);
         this.load.image('jello', jelloImg);
         this.load.image('coolwhip', coolwhipImg);
-
+        this.load.image('backboard', backboardImg);
+        this.load.image('hoop', hoopImg);
     }
       
     create ()
     {
-        
         var P1scoreText = 0;
         var P2scoreText = 0;
         P1scoreText = this.add.text(16, 16, 'P1 Score: 0', { fontSize: '32px', fill: '#000' });
         P2scoreText = this.add.text(750, 16, 'P2 Score: 0', { fontSize: '32px', fill: '#000' });
 
+        var backboard = this.add.image(500, 200, 'backboard');
+        backboard.setScale(0.3);
 
         var gravity = new b2Vec2(0,-15);
         window.world = this.myWorld = new b2World(gravity);
@@ -99,6 +103,50 @@ class MyGame extends Phaser.Scene
             })
         ];
 
+        //hoop
+
+        var hoop_shape = new b2PolygonShape();
+        hoop_shape.position.Set(0, 5);
+
+        hoop_shape.phaserSprite = this.add.image(0,0,'hoop');
+        hoop_shape.phaserSprite.setScale(0.3);
+
+        var hoop_vertices = hoop_shape.vertices;
+        hoop_vertices.push(new b2Vec2(-1.41, 3.9));
+        hoop_vertices.push(new b2Vec2(-1.13, 3.38));
+        hoop_vertices.push(new b2Vec2(-1.13, 4.48));
+        hoop_vertices.push(new b2Vec2(-1.41, 4.48));
+        ground.CreateFixtureFromShape(hoop_shape, 0);
+
+        var hoop_shape2 = new b2PolygonShape();
+        hoop_shape2.position.Set(0, 5);
+
+        var hoop_vertices2 = hoop_shape2.vertices;
+        hoop_vertices2.push(new b2Vec2(-1.13, 3.38));
+        hoop_vertices2.push(new b2Vec2(-0.45, 2.58));
+        hoop_vertices2.push(new b2Vec2(-1.13, 4.48));
+        ground.CreateFixtureFromShape(hoop_shape2, 0);
+
+        var hoop_shape3 = new b2PolygonShape();
+        hoop_shape3.position.Set(0, 5);
+
+        var hoop_vertices3 = hoop_shape3.vertices;
+        hoop_vertices3.push(new b2Vec2(1.13, 3.38));
+        hoop_vertices3.push(new b2Vec2(1.13, 4.48));
+        hoop_vertices3.push(new b2Vec2(0.45, 2.58));
+        ground.CreateFixtureFromShape(hoop_shape3, 0);
+
+        var hoop_shape4 = new b2PolygonShape();
+        hoop_shape4.position.Set(0, 5);
+
+        var hoop_vertices4 = hoop_shape4.vertices;
+        hoop_vertices4.push(new b2Vec2(1.41, 3.9));
+        hoop_vertices4.push(new b2Vec2(1.41, 4.48));
+        hoop_vertices4.push(new b2Vec2(1.13, 4.48));
+        hoop_vertices4.push(new b2Vec2(1.13, 3.38));
+        
+        ground.CreateFixtureFromShape(hoop_shape4, 0);
+
         //flipper
 
         var polygon_shape = new b2PolygonShape();
@@ -131,8 +179,6 @@ class MyGame extends Phaser.Scene
 
         var focalPoint = new b2Vec2(-7.8, 0.6); //got these numbers by clicking on the screen and copying coordinates for where i want the flipper to rotate around
         this.paddleMotor = paddleMotorDef.InitializeAndCreate(ground,body,focalPoint);
-
-        
 
         //flipper #2 
 
